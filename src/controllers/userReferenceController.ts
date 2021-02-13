c289721215a9e00bea5e4dbc76abe680517846b8
prashanthmsktm@gmail.com <servers@migobucks.com>
2021-02-13 11:04:21 +0530

    error handled

--- src/controllers/userReferenceController.ts
+++ src/controllers/userReferenceController.ts
@@ -5,46 +5,51 @@ import { HttpResponseMessage } from "../utils/httpResponseMessage";
 import { User } from "../models/userReference";
 
 export class UserReferenceController {
-    private constructor() {}
-  
-    private static instance: UserReferenceController = null;
-    private userReferenceService = null;
-  
-    /**
-     * To get singleton instance
-     *
-     * @param  {object} userReferenceService
-     */
-  
-    public static getInstance(
-        userReferenceService: IUserReferenceService = UserReferenceService.getInstance()
-    ) {
-      if (!UserReferenceController.instance) {
-        UserReferenceController.instance = new UserReferenceController();
-      }
-      UserReferenceController.instance.userReferenceService = userReferenceService; // mock service Object is passed as a param from .spec
-      return UserReferenceController.instance;
+  private constructor() {}
+
+  private static instance: UserReferenceController = null;
+  private userReferenceService = null;
+
+  /**
+   * To get singleton instance
+   *
+   * @param  {object} userReferenceService
+   */
+
+  public static getInstance(
+    userReferenceService: IUserReferenceService = UserReferenceService.getInstance()
+  ) {
+    if (!UserReferenceController.instance) {
+      UserReferenceController.instance = new UserReferenceController();
     }
-/**
+    UserReferenceController.instance.userReferenceService = userReferenceService; // mock service Object is passed as a param from .spec
+    return UserReferenceController.instance;
+  }
+  /**
    * Insert into user Reference table
    * TODO the functionality
    * @param  {object}   req
    * @param  {object}   res
    * @param  {function} next
    */
-  public async postUserReference(req: Request, res: Response, next: NextFunction) {
-    try{
+  public async postUserReference(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
       let userData: User = {
-          MobileNum: req.body.mobileNum
-        };
-      const result = await this.userReferenceService.postUserReference(userData);
-
-      if (result) {
-        HttpResponseMessage.successResponse(res, "Sucessfull");
+        MobileNum: req.body.mobileNum,
+      };
+      const result = await this.userReferenceService.postUserReference(
+        userData
+      );
+      if (result.errno) {
+        HttpResponseMessage.sendErrorResponse(res,result);
       } else {
-        HttpResponseMessage.sendErrorResponse(res, "Transaction Failed");
+        HttpResponseMessage.successResponse(res, "Sucessfull");
       }
-    }catch (err) {
+    } catch (err) {
       HttpResponseMessage.sendErrorResponse(res, err);
     }
   }
